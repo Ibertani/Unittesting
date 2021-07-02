2ac6c4a9764e4ecd7ef6a36d8b552b281362b77c
Isabela Bertani <isabelabertani33@gmail.com>
2021-07-02 11:43:35 -0400

    all tests done

--- sum.js
+++ sum.js
@@ -1,12 +1,19 @@
  
  
  function greet(name = "there!") {
-  
-    if (name.constructor === String && name === name.toUpperCase()) {
+   if (name.constructor === String && name === name.toUpperCase()) {
      return "HELLO" + " " + name + "!";
-   } else if (
-     name.constructor === Array) {
+   } else if (name.constructor === Array && name.length == 2) {
      return "Hello" + "," + name[0] + "," + name[1];
+   } else if (name.constructor === Array && name.length > 2) {
+    var sentence = '';
+    for (var i = 0; i < name.length; i++) {
+       var string = ",";
+       
+        sentence = sentence + string + name[i];
+       
+     }
+     return "Hello" + sentence;
    } else {
      return "Hello" + " " + name;
    }
--- sum.test.js
+++ sum.test.js
@@ -7,4 +7,6 @@ test ('greet name null val', () => {expect(greet()).toMatch('Hello there!')});
 
 test ('greet name in caps', () => {expect(greet('JOSE')).toMatch('HELLO JOSE!')});
 
-test ('greet name array', () => {expect (greet(['jose','pep'])).toMatch('Hello,jose,pep')});
+test ('greet name array small', () => {expect (greet(['jose','pep'])).toMatch('Hello,jose,pep')});
+
+test ('greet name array loop', () => {expect(greet(['jose', 'pep', 'blah', 'boo'])).toMatch('Hello,jose,pep,blah,boo')});
